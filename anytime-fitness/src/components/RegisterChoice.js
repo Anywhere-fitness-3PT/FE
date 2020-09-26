612ce3e47b031fb99b3e141f22492218c106d667
gitnubl33t <jessicad8695@gmail.com>
2020-09-25 19:52:57 -0700

    added RegisterChoice component with  buttons to signup as trainee or instructor

---
+++ anytime-fitness/src/components/RegisterChoice.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function RegisterChoice() {
+  return (
+    <div className="register-landing">
+      <h1>Are you a Trainee?</h1>
+      <button>Register here</button>
+      <h1>Are you an Instructor?</h1>
+      <button>Register here</button>
+    </div>
+  );
+}
---
+++ anytime-fitness/src/components/SignUpForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+import RegisterChoice from "./RegisterChoice";
+
+export default function SignUpForm() {
+  return (
+    <div>
+      <RegisterChoice />
+    </div>
+  );
+}
